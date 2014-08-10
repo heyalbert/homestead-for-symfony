16646d09f0d5d804eba07376a251772c9f1e9739
anthonymartin <anthony@replaycreative.com>
2014-08-10 16:02:49 -0400

    Update serve.sh

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -23,11 +23,12 @@ block="server {
 
     sendfile off;
 
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    location ~ ^/(app|app_dev|config)\.php(/|$) {
         fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
+        fastcgi_split_path_info ^(.+\.php)(/.*)$;
         include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param HTTPS off;
     }
 
     location ~ /\.ht {
